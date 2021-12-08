b02becbc14aa517b87952ad61fc6b44928e29ddd
JamesJSullivan <91743888+JamesJSullivan@users.noreply.github.com>
2021-12-08 17:58:48 -0500

    Create command_test.java

---
+++ src/backendsrc/command_test.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.io.File;
+import java.io.IOException;
+
+public class command_test {
+
+    public static void main(String[] arg) throws InterruptedException {
+        String dir = System.getProperty("user.dir");
+        String abspath = dir + File.separator + "command.sh";
+        ProcessBuilder pb = new ProcessBuilder(abspath);
+        Process p;
+        try {
+            p = pb.start();
+            p.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+    }
+
+}
